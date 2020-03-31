872c485a1a21a15a3b8429f231f9660704c7b439
Abdulmalik <abdulmmbs@gmail.com>
2020-03-31 01:53:05 +0100

    Initial Commit

---
+++ ContactsLikeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactsLikeApp
+//
+//  Created by Mo Abdulmalik on 31/03/2020.
+//  Copyright © 2020 Aoomle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
